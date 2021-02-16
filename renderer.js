42fe2ad3633b2c13d5ab5faf8ec67643059b11f2
zao95 <zao95@naver.com>
2021-02-16 13:15:02 +0900

    Update renderer

--- renderer.js
+++ renderer.js
@@ -6,58 +6,52 @@ const sass = require('sass')
 const sourceDir = "src"
 const distDir = "dist"
 
-const htmlRender = async () => {
-    const files = await glob.sync(`./${sourceDir}/**/*.html`)
-    for (let file of files) {
-        const savePath = file.replace(sourceDir, distDir)
-        const data = await fs.readFile(file, 'utf8')
-        const pathDir = path.dirname(file).replace(sourceDir, distDir)
-        try { await fs.access(pathDir) } catch (e) {
-            await fs.mkdir(pathDir.replace(sourceDir, distDir), { recursive: true })
-        }
-        try { await fs.access(savePath) } catch (e) {
-            await fs.writeFile(savePath.replace(sourceDir, distDir), data)
-            console.log(`${file} has been copied to ${savePath}`)
-        }
+const htmlRender = async (file) => {
+    const savePath = file.replace(sourceDir, distDir)
+    const data = await fs.readFile(file, 'utf8')
+    const pathDir = path.dirname(file).replace(sourceDir, distDir)
+    try { await fs.access(pathDir) } catch (e) {
+        await fs.mkdir(pathDir.replace(sourceDir, distDir), { recursive: true })
     }
+    await fs.writeFile(savePath.replace(sourceDir, distDir), data)
+    console.log(`${file} has been copied to ${savePath}`)
 }
 
-const styleRender = async () => {
-    const files = await glob.sync(`./${sourceDir}/**/*.sass`)
-    for (let file of files) {
-        const pathDir = path.dirname(file).replace(sourceDir, distDir)
-        const savePath = file.replace(sourceDir, distDir).slice(0, -4).concat("css")
-        const data = await sass.renderSync({file: file}).css.toString('utf8')
-        try { await fs.access(pathDir) } catch (e) {
-            await fs.mkdir(pathDir.replace(sourceDir, distDir), { recursive: true })
-        }
-        try { await fs.access(savePath) } catch (e) {
-            await fs.writeFile(savePath.replace(sourceDir, distDir), data)
-            console.log(`${file} has been copied to ${savePath}`)
-        }
+const styleRender = async (file) => {
+    const pathDir = path.dirname(file).replace(sourceDir, distDir)
+    const savePath = file.replace(sourceDir, distDir).slice(0, -4).concat("css")
+    const data = await sass.renderSync({file: file}).css.toString('utf8')
+    try { await fs.access(pathDir) } catch (e) {
+        await fs.mkdir(pathDir.replace(sourceDir, distDir), { recursive: true })
     }
+    await fs.writeFile(savePath.replace(sourceDir, distDir), data)
+    console.log(`${file} has been copied to ${savePath}`)
 }
 
-const scriptRender = async () => {
-    const files = await glob.sync(`./${sourceDir}/**/*.js`)
-    for (let file of files) {
-        const savePath = file.replace(sourceDir, distDir)
-        const data = await fs.readFile(file, 'utf8')
-        const pathDir = path.dirname(file).replace(sourceDir, distDir)
-        try { await fs.access(pathDir) } catch (e) {
-            await fs.mkdir(pathDir.replace(sourceDir, distDir), { recursive: true })
-        }
-        try { await fs.access(savePath) } catch (e) {
-            await fs.writeFile(savePath.replace(sourceDir, distDir), data)
-            console.log(`${file} has been copied to ${savePath}`)
-        }
+const scriptRender = async (file) => {
+    const savePath = file.replace(sourceDir, distDir)
+    const data = await fs.readFile(file, 'utf8')
+    const pathDir = path.dirname(file).replace(sourceDir, distDir)
+    try { await fs.access(pathDir) } catch (e) {
+        await fs.mkdir(pathDir.replace(sourceDir, distDir), { recursive: true })
     }
+    await fs.writeFile(savePath.replace(sourceDir, distDir), data)
+    console.log(`${file} has been copied to ${savePath}`)
 }
 
 const renderer = () => {
-    htmlRender()
-    styleRender()
-    scriptRender()
+    const htmls = glob.sync(`./${sourceDir}/**/*.html`)
+    for (let html of htmls) {
+        htmlRender(html)
+    }
+    const styles = glob.sync(`./${sourceDir}/**/*.sass`)
+    for (let style of styles) {
+        styleRender(style)
+    }
+    const scripts = glob.sync(`./${sourceDir}/**/*.js`)
+    for (let script of scripts) {
+        scriptRender(script)
+    }
 }
 
 try {
